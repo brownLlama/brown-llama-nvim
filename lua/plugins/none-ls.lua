e8930a2e43741e54799546f0b471cd003e912428
brownLlama <inbox.sanu@gmail.com>
2024-01-01 17:26:14 +0100

    changee diagnostics from flake8 to ruff

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,9 +9,9 @@ return {
 
         -- Python
         null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.diagnostics.pydocstyle,
+        null_ls.builtins.diagnostics.ruff,
 
         -- Json
         null_ls.builtins.diagnostics.jsonlint,
