3b53f0c923330694d1af0f6d045fb9f4f9101e80
brownLlama <inbox.sanu@gmail.com>
2024-01-02 18:14:41 +0100

    no horizontal lines from indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,5 +1,11 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  opts = {},
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {},
+	config = function()
+		local ibl = require("ibl")
+		ibl.setup({
+			scope = { enabled = false },
+		})
+	end,
 }
