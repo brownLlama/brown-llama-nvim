ea3deeedc0adbdd3c50459cc82b89604acb35c9f
brownLlama <inbox.sanu@gmail.com>
2024-01-03 22:44:56 +0100

    changed from meta key to alt key

--- lua/plugins/mini-move.lua
+++ lua/plugins/mini-move.lua
@@ -1,28 +1,28 @@
 return {
-  "echasnovski/mini.move",
-  version = false,
-  config = function()
-    require("mini.move").setup({
-      -- Module mappings. Use `''` (empty string) to disable one.
-      mappings = {
-        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-        left = "<M-h>",
-        right = "<M-l>",
-        down = "<M-j>",
-        up = "<M-k>",
+	"echasnovski/mini.move",
+	version = false,
+	config = function()
+		require("mini.move").setup({
+			-- Module mappings. Use `''` (empty string) to disable one.
+			mappings = {
+				-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+				left = "<A-h>",
+				right = "<A-l>",
+				down = "<A-j>",
+				up = "<A-k>",
 
-        -- Move current line in Normal mode
-        line_left = "<M-h>",
-        line_right = "<M-l>",
-        line_down = "<M-j>",
-        line_up = "<M-k>",
-      },
+				-- Move current line in Normal mode
+				line_left = "<A-h>",
+				line_right = "<A-l>",
+				line_down = "<A-j>",
+				line_up = "<A-k>",
+			},
 
-      -- Options which control moving behavior
-      options = {
-        -- Automatically reindent selection during linewise vertical move
-        reindent_linewise = true,
-      },
-    })
-  end,
+			-- Options which control moving behavior
+			options = {
+				-- Automatically reindent selection during linewise vertical move
+				reindent_linewise = true,
+			},
+		})
+	end,
 }
