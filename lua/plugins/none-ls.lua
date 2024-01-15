c1981f35ed81da9f5edd02b77764e2e546891d66
brownLlama <inbox.sanu@gmail.com>
2024-01-15 10:38:05 +0100

    terraform added to none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,36 +1,37 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.terraform_fmt,
 
-				-- Python
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.diagnostics.mypy,
-				null_ls.builtins.diagnostics.pydocstyle,
-				null_ls.builtins.diagnostics.ruff,
+        -- Python
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.pydocstyle,
+        null_ls.builtins.diagnostics.ruff,
 
-				-- Json
-				null_ls.builtins.diagnostics.jsonlint,
+        -- Json
+        null_ls.builtins.diagnostics.jsonlint,
 
-				-- YAML
-				null_ls.builtins.diagnostics.yamllint,
-			},
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format()
-						end,
-					})
-				end
-			end,
-		})
-	end,
+        -- YAML
+        null_ls.builtins.diagnostics.yamllint,
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format()
+            end,
+          })
+        end
+      end,
+    })
+  end,
 }
