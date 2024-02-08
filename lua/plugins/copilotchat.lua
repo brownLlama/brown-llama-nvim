7480a69eda784e899b6b493e39349dab543bfd0b
brownLlama <inbox.sanu@gmail.com>
2024-02-08 17:33:07 +0100

    copilotchat added

---
+++ lua/plugins/copilotchat.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      show_help = "yes",      -- Show help text for CopilotChatInPlace, default: yes
+      debug = false,          -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
+      disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
+    },
+    build = function()
+      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+    end,
+    event = "VeryLazy",
+  },
+}
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,74 +1,88 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  config = function()
-    local wk = require("which-key")
-    wk.register({
-      ["<leader>"] = {
-        -- Toggle Explorer
-        ["e"] = { "<cmd>Neotree toggle right<cr>", "Toggle Explorer" },
-        -- Markdown Preview
-        ["m"] = { "<cmd>MPOpen<cr>", "Markdown Preview" },
-        -- Exit Nvim
-        ["q"] = { "<cmd>q<cr>", "Exit" },
-        -- ToggleTerm
-        ["t"] = { "<cmd>ToggleTerm<cr>", "Toggle Terminal" },
-        -- Vertical Split
-        ["v"] = { "<cmd>vsplit<cr>", "Vertical Split" },
-        -- Write
-        ["w"] = { "<cmd>w<cr>", "Write" },
-        -- Close & Exit Buffer
-        ["x"] = { "<cmd>bw<cr>", "Close Buffer" },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	config = function()
+		local wk = require("which-key")
+		wk.register({
+			["<leader>"] = {
+				-- Toggle Explorer
+				["e"] = { "<cmd>Neotree toggle right<cr>", "Toggle Explorer" },
+				-- Markdown Preview
+				["m"] = { "<cmd>MPOpen<cr>", "Markdown Preview" },
+				-- Exit Nvim
+				["q"] = { "<cmd>q<cr>", "Exit" },
+				-- Vertical Split
+				["v"] = { "<cmd>vsplit<cr>", "Vertical Split" },
+				-- Write
+				["w"] = { "<cmd>w<cr>", "Write" },
+				-- Close & Exit Buffer
+				["x"] = { "<cmd>bw<cr>", "Close Buffer" },
 
-        -- Telescope
-        f = {
-          name = "Telescope", -- Group name for Telescope related commands
-          f = { "<cmd>Telescope find_files<cr>", "Find Files" },
-          g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-          b = { "<cmd>Telescope buffers<cr>", "Buffers" },
-          w = { "<cmd>Telescope grep_string<cr>", "Current Word" },
-        },
+				-- Telescope
+				f = {
+					name = "Telescope", -- Group name for Telescope related commands
+					f = { "<cmd>Telescope find_files<cr>", "Find Files" },
+					g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
+					b = { "<cmd>Telescope buffers<cr>", "Buffers" },
+					w = { "<cmd>Telescope grep_string<cr>", "Current Word" },
+				},
 
-        -- Git
-        g = {
-          name = "Git",
-          g = { "<cmd>Flog<cr>", "Git Graph" },
-          l = { "<cmd>LazyGit<cr>", "LazyGit" },
-        },
+				-- Git
+				g = {
+					name = "Git",
+					g = { "<cmd>Flog<cr>", "Git Graph" },
+					l = { "<cmd>LazyGit<cr>", "LazyGit" },
+				},
 
-        -- Flow
-        p = {
-          name = "Flow",
-          r = { "<cmd>FlowRunFile<cr>", "Run File" },
-          s = { "<cmd>FlowRunSelected<cr>", "Run Selected" },
-        },
+				-- Toggle Options
+				t = {
+					name = "Toggle Options",
+					t = { "<cmd>ToggleTerm<cr>", "Terminal" },
+					d = { ":DiagnosticsToggleVirtualText<cr>", "Diagnostics" },
+				},
 
-        -- Toggle Options
-        t = {
-          name = "Toggle Options",
-          t = { "<cmd>ToggleTerm<cr>", "Terminal" },
-          d = { ":DiagnosticsToggleVirtualText<cr>", "Diagnostics" },
-        },
+				-- Rotate Windows
+				o = {
+					name = "Other Features",
+					w = { "<cmd>RotateWindows<cr>", "Rotate Windows" },
+					l = { ":sil !open <cWORD><cr>", "Open Link" },
+				},
 
-        -- Rotate Windows
-        o = {
-          name = "Other Features",
-          w = { "<cmd>RotateWindows<cr>", "Rotate Windows" },
-          l = { ":sil !open <cWORD><cr>", "Open Link" },
-        },
+				-- CopilotChat
+				c = {
+					name = "CopilotChat",
+					e = { "<cmd>CopilotChatExplain<cr>", "Explain" },
+				},
 
-        -- Neorg
-        -- n = {
-        --   name = "Neorg",
-        --   s = { "<cmd>Neorg sync-parsers<cr>", "Sync Parsers" },
-        --   b = { "<cmd>Neorg workspace brownllama<cr>", "brownLlama Workspace" },
-        --   d = { "<cmd>Neorg workspace datadice<cr>", "datadice Workspace" },
-        -- },
-      },
-    })
-  end,
+				-- -- Flow
+				-- p = {
+				--   name = "Flow",
+				--   r = { "<cmd>FlowRunFile<cr>", "Run File" },
+				--   s = { "<cmd>FlowRunSelected<cr>", "Run Selected" },
+				-- },
+
+				-- Neorg
+				-- n = {
+				--   name = "Neorg",
+				--   s = { "<cmd>Neorg sync-parsers<cr>", "Sync Parsers" },
+				--   b = { "<cmd>Neorg workspace brownllama<cr>", "brownLlama Workspace" },
+				--   d = { "<cmd>Neorg workspace datadice<cr>", "datadice Workspace" },
+				-- },
+			},
+		})
+
+		-- Visual Mode Commands
+		wk.register({
+			-- CopilotChat
+			c = {
+				name = "Copilot Chat", -- Display name when pressing <leader>c
+				v = { ":CopilotChatVisual ", "Visual Chat", mode = "x" },
+				i = { ":CopilotChatInPlace<cr>", "In-place code", mode = "x" },
+			},
+		}, { mode = "x", prefix = "<leader>" })
+	end,
 }
