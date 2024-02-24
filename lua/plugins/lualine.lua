aa99bc3b16aa983eeb02bdc3af2828997dfd8a02
brownLlama <inbox.sanu@gmail.com>
2024-02-24 19:35:59 +0100

    theme changed to everforest

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,19 +1,19 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "catppuccin",
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch" },
-				lualine_c = {},
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = {},
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "nord",
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+    })
+  end,
 }
---
+++ lua/plugins/nord.lua
@@ -0,0 +1,9 @@
+return {
+  "shaunsingh/nord.nvim",
+  lazy = false,
+  name = "nord",
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme("nord")
+  end,
+}
