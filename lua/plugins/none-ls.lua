ad8e159f92bab4a5dceaad900acab42f1c70b8f3
brownLlama <inbox.sanu@gmail.com>
2024-02-26 12:18:04 +0100

    updating none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,38 +1,82 @@
 return {
-	"nvimtools/none-ls.nvim",
+	"nvimtools/none-ls.nvim", -- configure formatters & linters
+	lazy = true,
+	-- event = { "BufReadPre", "BufNewFile" }, -- to enable uncomment this
+	dependencies = {
+		"jay-babu/mason-null-ls.nvim",
+	},
 	config = function()
+		local mason_null_ls = require("mason-null-ls")
+
 		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.terraform_fmt,
 
-				-- Python
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.shfmt,
-				null_ls.builtins.diagnostics.pydocstyle,
-				null_ls.builtins.diagnostics.ruff,
-				null_ls.builtins.diagnostics.flake8,
-				null_ls.builtins.diagnostics.mypy,
-				-- null_ls.builtins.diagnostics.pylint,
-				null_ls.builtins.diagnostics.shellcheck,
+		local null_ls_utils = require("null-ls.utils")
 
-				-- Json
-				null_ls.builtins.diagnostics.jsonlint,
-
-				-- YAML
-				null_ls.builtins.diagnostics.yamllint,
+		mason_null_ls.setup({
+			ensure_installed = {
+				"stylua", -- lua formatter
+				"prettier", -- prettier formatter
+				"terraform_fmt", -- terraform formatter
+				"black", -- python formatter
+				"isort", -- python formatter
+				"shfmt", -- shell formatter
+				"pydocstyle", -- python linter
+				"flake8", -- python linter
+				"ruff", -- python linter
+				"mypy", -- python linter
+				"shellcheck", -- shell linter
+				"terraform_lint", -- terraform linter
+				"jsonlint", -- json linter
+				"yamllint", -- yaml linter
 			},
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
+		})
+
+		-- for conciseness
+		local formatting = null_ls.builtins.formatting -- to setup formatters
+		local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+
+		-- to setup format on save
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+		-- configure null_ls
+		null_ls.setup({
+			-- add package.json as identifier for root (for typescript monorepos)
+			root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
+			-- setup formatters & linters
+			sources = {
+				--  to disable file types use
+				--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
+				formatting.stylua, -- lua formatter
+				formatting.prettier, -- js/ts formatter
+				formatting.terraform_fmt, -- terraform formatter
+				formatting.black, -- python formatter
+				formatting.isort, -- python formatter
+				formatting.shfmt, -- shell formatter
+
+				diagnostics.pydocstyle, -- python linter
+				diagnostics.flake8, -- python linter
+				diagnostics.ruff, -- python linter
+				diagnostics.mypy, -- python linter
+				diagnostics.shellcheck, -- shell linter
+				diagnostics.terraform_lint, -- terraform linter
+				diagnostics.jsonlint, -- json linter
+				diagnostics.yamllint, -- yaml linter
+			},
+			-- configure format on save
+			on_attach = function(current_client, bufnr)
+				if current_client.supports_method("textDocument/formatting") then
 					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 					vim.api.nvim_create_autocmd("BufWritePre", {
 						group = augroup,
 						buffer = bufnr,
 						callback = function()
-							vim.lsp.buf.format()
+							vim.lsp.buf.format({
+								filter = function(client)
+									--  only use null-ls for formatting instead of lsp server
+									return client.name == "null-ls"
+								end,
+								bufnr = bufnr,
+							})
 						end,
 					})
 				end
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,106 +1,106 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  config = function()
-    local wk = require("which-key")
-    wk.register({
-      ["<leader>"] = {
-        -- Toggle Explorer
-        ["e"] = { "<cmd>Neotree toggle right<cr>", "Toggle Explorer" },
-        -- Markdown Preview
-        ["m"] = { "<cmd>MPOpen<cr>", "Markdown Preview" },
-        -- Exit Nvim
-        ["q"] = { "<cmd>q<cr>", "Exit" },
-        -- Vertical Split
-        ["v"] = { "<cmd>vsplit<cr>", "Vertical Split" },
-        -- Write
-        ["w"] = { "<cmd>w<cr>", "Write" },
-        -- Close & Exit Buffer
-        ["x"] = { "<cmd>bw<cr>", "Close Buffer" },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	config = function()
+		local wk = require("which-key")
+		wk.register({
+			["<leader>"] = {
+				-- Toggle Explorer
+				["e"] = { "<cmd>Neotree toggle right<cr>", "Toggle Explorer" },
+				-- Markdown Preview
+				["m"] = { "<cmd>MPOpen<cr>", "Markdown Preview" },
+				-- Exit Nvim
+				["q"] = { "<cmd>q<cr>", "Exit" },
+				-- Vertical Split
+				["v"] = { "<cmd>vsplit<cr>", "Vertical Split" },
+				-- Write
+				["w"] = { "<cmd>w<cr>", "Write" },
+				-- Close & Exit Buffer
+				["x"] = { "<cmd>bw<cr>", "Close Buffer" },
 
-        -- Telescope
-        f = {
-          name = "Telescope", -- Group name for Telescope related commands
-          f = { "<cmd>Telescope find_files<cr>", "Find Files" },
-          g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-          b = { "<cmd>Telescope buffers<cr>", "Buffers" },
-          w = { "<cmd>Telescope grep_string<cr>", "Current Word" },
-        },
+				-- Telescope
+				f = {
+					name = "Telescope", -- Group name for Telescope related commands
+					f = { "<cmd>Telescope find_files<cr>", "Find Files" },
+					g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
+					b = { "<cmd>Telescope buffers<cr>", "Buffers" },
+					w = { "<cmd>Telescope grep_string<cr>", "Current Word" },
+				},
 
-        -- Git
-        g = {
-          name = "Git",
-          g = { "<cmd>Flog<cr>", "Git Graph" },
-          l = { "<cmd>LazyGit<cr>", "LazyGit" },
-          b = { "<cmd>Gitsigns toggle_current_line_blame<cr>", "Blame Current Line" },
-        },
+				-- Git
+				g = {
+					name = "Git",
+					g = { "<cmd>Flog<cr>", "Git Graph" },
+					l = { "<cmd>LazyGit<cr>", "LazyGit" },
+					b = { "<cmd>Gitsigns toggle_current_line_blame<cr>", "Blame Current Line" },
+				},
 
-        -- Toggle Options
-        t = {
-          name = "Toggle Options",
-          t = { "<cmd>ToggleTerm<cr>", "Terminal" },
-          d = { ":DiagnosticsToggleVirtualText<cr>", "Diagnostics" },
-        },
+				-- Toggle Options
+				t = {
+					name = "Toggle Options",
+					t = { "<cmd>ToggleTerm<cr>", "Terminal" },
+					d = { ":DiagnosticsToggleVirtualText<cr>", "Diagnostics" },
+				},
 
-        -- Rotate Windows
-        o = {
-          name = "Other Features",
-          w = { "<cmd>RotateWindows<cr>", "Rotate Windows" },
-          l = { ":sil !open <cWORD><cr>", "Open Link" },
-        },
+				-- Rotate Windows
+				o = {
+					name = "Other Features",
+					w = { "<cmd>RotateWindows<cr>", "Rotate Windows" },
+					l = { ":sil !open <cWORD><cr>", "Open Link" },
+				},
 
-        -- CopilotChat
-        c = {
-          name = "CopilotChat",
-          e = { "<cmd>CopilotChatExplain<cr>", "Explain" },
-        },
+				-- CopilotChat
+				c = {
+					name = "CopilotChat",
+					e = { "<cmd>CopilotChatExplain<cr>", "Explain" },
+				},
 
-        -- -- Flow
-        -- p = {
-        --   name = "Flow",
-        --   r = { "<cmd>FlowRunFile<cr>", "Run File" },
-        --   s = { "<cmd>FlowRunSelected<cr>", "Run Selected" },
-        -- },
+				-- -- Flow
+				-- p = {
+				--   name = "Flow",
+				--   r = { "<cmd>FlowRunFile<cr>", "Run File" },
+				--   s = { "<cmd>FlowRunSelected<cr>", "Run Selected" },
+				-- },
 
-        -- Neorg
-        -- n = {
-        --   name = "Neorg",
-        --   s = { "<cmd>Neorg sync-parsers<cr>", "Sync Parsers" },
-        --   b = { "<cmd>Neorg workspace brownllama<cr>", "brownLlama Workspace" },
-        --   d = { "<cmd>Neorg workspace datadice<cr>", "datadice Workspace" },
-        -- },
-      },
-    })
+				-- Neorg
+				-- n = {
+				--   name = "Neorg",
+				--   s = { "<cmd>Neorg sync-parsers<cr>", "Sync Parsers" },
+				--   b = { "<cmd>Neorg workspace brownllama<cr>", "brownLlama Workspace" },
+				--   d = { "<cmd>Neorg workspace datadice<cr>", "datadice Workspace" },
+				-- },
+			},
+		})
 
-    -- Visual Mode Commands
-    wk.register({
-      -- CopilotChat
-      c = {
-        name = "Copilot Chat", -- Display name when pressing <leader>c
-        v = { ":CopilotChatVisual ", "Visual Chat", mode = "x" },
-        i = { ":CopilotChatInPlace<cr>", "In-place code", mode = "x" },
-      },
-    }, { mode = "x", prefix = "<leader>" })
+		-- Visual Mode Commands
+		wk.register({
+			-- CopilotChat
+			c = {
+				name = "Copilot Chat", -- Display name when pressing <leader>c
+				v = { ":CopilotChatVisual ", "Visual Chat", mode = "x" },
+				i = { ":CopilotChatInPlace<cr>", "In-place code", mode = "x" },
+			},
+		}, { mode = "x", prefix = "<leader>" })
 
-    wk.register({
-      c = {
-        name = "Copilot", -- Optional: gives a name to this group of commands
-        c = {
-          function()
-            local input = vim.fn.input("Copilot Query: ")
-            -- Ensure input is not empty before proceeding
-            if input ~= "" then
-              -- Execute the CopilotChat command with the user's input
-              vim.cmd("CopilotChat " .. input)
-            end
-          end,
-          "Copilot Chat",
-        },
-      },
-    }, { prefix = "<leader>" })
-  end,
+		wk.register({
+			c = {
+				name = "Copilot", -- Optional: gives a name to this group of commands
+				c = {
+					function()
+						local input = vim.fn.input("Copilot Query: ")
+						-- Ensure input is not empty before proceeding
+						if input ~= "" then
+							-- Execute the CopilotChat command with the user's input
+							vim.cmd("CopilotChat " .. input)
+						end
+					end,
+					"Copilot Chat",
+				},
+			},
+		}, { prefix = "<leader>" })
+	end,
 }
