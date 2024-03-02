5f26a4d8b96071016dd12396635cc6d804078b91
brownLlama <inbox.sanu@gmail.com>
2024-03-02 13:43:11 +0100

    auto close explorer is disabled

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,31 +1,32 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      close_if_last_window = true,
-      filesystem = {
-        filtered_items = {
-          visible = true,
-        },
-      },
-      event_handlers = {
-        {
-          event = "file_opened",
-          handler = function()
-            require("neo-tree.command").execute({ action = "close" })
-          end,
-        },
-      },
-      source_selector = {
-        winbar = false,
-        statusline = false,
-      },
-    })
-  end,
+	-- "nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			filesystem = {
+				filtered_items = {
+					visible = true,
+				},
+			},
+			event_handlers = {
+				-- Closes the explorer when a file is opened
+				-- {
+				-- 	event = "file_opened",
+				-- 	handler = function()
+				-- 		require("neo-tree.command").execute({ action = "close" })
+				-- 	end,
+				-- },
+			},
+			source_selector = {
+				winbar = false,
+				statusline = false,
+			},
+		})
+	end,
 }
